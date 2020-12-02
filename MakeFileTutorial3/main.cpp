ef79252c6d7d1e2d0b47ecda40e49186a6c05956
Thien <thienxuanle@yahoo.com>
2020-12-01 19:54:11 -0500

    makefile

---
+++ MakeFileTutorial3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main(){
+    print_hello();
+    cout << endl;
+    cout << "The factorial of 10 is " << factorial(10) << endl;
+    return 0;
+}
